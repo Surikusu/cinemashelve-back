4dcc34acf641d471fb30b516640f1eaabc1c56cc
Robinson <robyvuillemin@gmail.com>
2024-07-05 15:05:02 +0200

    premier setup

---
+++ models/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(connectionString, {connectTimeoutMS: 2000})
+.then(() => console.log('Database connected'))
+.catch(error => console.error(error))
+
+module.exports = connectionString;
